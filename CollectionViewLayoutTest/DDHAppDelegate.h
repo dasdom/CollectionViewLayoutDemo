789432cd7729fb68207d2f2766670159491f6444
dasdom <dom@dasdom.de>
2014-01-05 15:36:49 +0100

    Initial Commit

---
+++ CollectionViewLayoutTest/DDHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDHAppDelegate.h
+//  CollectionViewLayoutTest
+//
+//  Created by dasdom on 05.01.14.
+//  Copyright (c) 2014 dasdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
